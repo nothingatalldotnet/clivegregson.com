491256e0c3c4ef84b4ef31f1bb2561699f275ac9
rich <rich@nothingatall.net>
2021-06-27 14:41:07 +0100

    single disco

--- wp-content/themes/baltazar-lite-child/single-discography.php
+++ wp-content/themes/baltazar-lite-child/single-discography.php
@@ -3,16 +3,39 @@
 	if(have_posts()) {
 		while (have_posts()) {
 			the_post(); 
+			$post_id = get_the_ID();
 			$post_title = get_the_title();
 			$post_release = get_field('release_date');
 			$post_artist = get_field('artist-band');
 			$post_tracklist = get_field('tracklisting');
-			$post_image = get_the_post_thumbnail_url($this_id, 'discog-thumbnail');
+			$post_image = get_the_post_thumbnail_url($post_id, 'discog-thumbnail');
 ?>
 <style>
 	#breadcrumbs {
 		border-bottom: solid #292929;
 		margin-bottom: 12px !important;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		width: 100%;
+	}
+
+	.col {
+		display: flex;
+		flex-direction: column;
+		flex-basis: 100%;
+		flex: 1;
+	}
+
+	.col img {
+		margin-left: 20px;
+	}
+
+	ul {
+		list-style: none;
 	}
 </style>
 
@@ -27,24 +50,42 @@
 	}
 
 	echo '<h1>'.$post_artist.' - '.$post_title.'</h1>';
-	echo '<img src="'.$post_image.'" style="float:right;">';
-	echo '<div style="float:left;">';
-	the_content();
-	echo '</div>';
-	echo '<br style="clear:both;">';
 
+	echo '<div class="row">';
 	if(have_rows('tracklisting')) {
-		echo '<h2>Tracklisting</h2>';
-		echo '<ul>';
+		echo '<div class="col">';
+		echo '	<h2>Tracklisting</h2>';
+		echo '	<ul>';
 		while(have_rows('tracklisting') ) {
 			the_row();
-			$postition = get_sub_field('position');
+			$position = get_sub_field('position');
 			$title = get_sub_field('title');
-			
 			echo '<li><span>'.$position.'</span> - '.$title.'</li>';
 		}
-		echo '</ul>';
+		echo '	</ul>';
+		echo '</div>';
+		echo '<div class="col">';
+		echo '	<img src="'.$post_image.'">';
+		echo '</div>';
+
+		the_content();
+
+	} else {
+		echo '<div class="col">';
+		the_content();
+		echo '</div>';
+		echo '<div class="col">';
+		echo '	<img src="'.$post_image.'">';
+		echo '</div>';
 	}
+	echo '</div>';
+
+	
+
+
+
+
+
 
 	if(have_rows('links')) {
 		echo '<h2>Useful Links</h2>';
